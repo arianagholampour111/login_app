0cb9166f23781aa6be8ebca954adbf8566e7e19a
ArianaGholampour <arianagholampour111@gmail.com>
2025-09-15 18:40:18 +0330

    Update database_management.py

--- LOGIN_APP/data_access/database_management.py
+++ LOGIN_APP/data_access/database_management.py
@@ -2,10 +2,11 @@ import sqlite3
 def create_table_users():
     connection = sqlite3.connect("users.db")
     cursor = connection.cursor()
-    cursor.execute("""create table if not exists users(id integer primary key autoincrement,
+    cursor.execute("""create users(id integer primary key autoincrement,
     username text,
     password text,
     nickname text,
     lock_state integer)""")
     connection.commit()
+
     connection.close()
